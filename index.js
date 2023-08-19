caa645d3ed4eccb1d8fc1c12fe33921e1afe50e0
azuredevil053 <colton.fullstackdev053@gmail.com>
2023-08-19 21:42:27 +0900

    udpate

--- index.js
+++ index.js
@@ -18,57 +18,57 @@ const postsRouter = require("./routes/posts");
 require('dotenv').config();
 
 
-// app
-const app = express();
+// // app
+// const app = express();
 
-// db connection
-const connectDB = async () => {
-  try {
-    await mongoose.connect(
-      process.env.MONGODB_URI,
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
-        useFindAndModify: false,
-      }
-    );
-    console.log('MongoDB Connected');
-  } catch (err) {
-    console.error(err.message);
-    // exit process with failure
-    process.exit(1);
-  }
-};
-connectDB();
+// // db connection
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect(
+//       process.env.MONGODB_URI,
+//       {
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true,
+//         useCreateIndex: true,
+//         useFindAndModify: false,
+//       }
+//     );
+//     console.log('MongoDB Connected');
+//   } catch (err) {
+//     console.error(err.message);
+//     // exit process with failure
+//     process.exit(1);
+//   }
+// };
+// connectDB();
 
-// middlewares
-app.use(morgan('dev'));
-app.use(bodyParser.json());
-app.use(cookieParser());
-app.use(expressValidator());
-app.use(cors());
+// // middlewares
+// app.use(morgan('dev'));
+// app.use(bodyParser.json());
+// app.use(cookieParser());
+// app.use(expressValidator());
+// app.use(cors());
 
-// routes middleware
-app.use('/api', authRoutes);
-app.use('/api', userRoutes);
-app.use('/api', categoryRoutes);
-app.use('/api', productRoutes);
-app.use('/api', braintreeRoutes);
-app.use('/api', orderRoutes);
-app.use('/api/posts', postsRouter);
+// // routes middleware
+// app.use('/api', authRoutes);
+// app.use('/api', userRoutes);
+// app.use('/api', categoryRoutes);
+// app.use('/api', productRoutes);
+// app.use('/api', braintreeRoutes);
+// app.use('/api', orderRoutes);
+// app.use('/api/posts', postsRouter);
 
-// Server static assets if in production
-if (process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use(express.static('client/build'));
+// // Server static assets if in production
+// if (process.env.NODE_ENV === 'production') {
+//   // Set static folder
+//   app.use(express.static('client/build'));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+//   });
+// }
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 
 app.get('/', (req, res) => {
     console.log("Just got a request!")
