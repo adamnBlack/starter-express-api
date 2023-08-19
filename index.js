47a8fb04f35e60e93d51cf9e3c5aa69699eed15b
azuredevil053 <colton.fullstackdev053@gmail.com>
2023-08-19 21:28:14 +0900

    update index

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
 const express = require('express')
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const path = require('path');
+const expressValidator = require('express-validator');
+
+const authRoutes = require('./routes/auth');
+const userRoutes = require('./routes/user');
+const categoryRoutes = require('./routes/category');
+const productRoutes = require('./routes/product');
+const braintreeRoutes = require('./routes/braintree');
+const orderRoutes = require('./routes/order');
+const postsRouter = require("./routes/posts");
+
+require('dotenv').config();
 const app = express()
 app.all('/', (req, res) => {
     console.log("Just got a request!")
