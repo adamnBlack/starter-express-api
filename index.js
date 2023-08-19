158d384cbbb294d8fa149bc9d65ca93876f43885
azuredevil053 <colton.fullstackdev053@gmail.com>
2023-08-19 22:04:20 +0900

    update index.js

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ const path = require('path');
 const expressValidator = require('express-validator');
 
 //const authRoutes = require('./routes/auth');
-const userRoutes = require('./routes/user');
-const categoryRoutes = require('./routes/category');
-const productRoutes = require('./routes/product');
-const braintreeRoutes = require('./routes/braintree');
-const orderRoutes = require('./routes/order');
+//const userRoutes = require('./routes/user');
+// const categoryRoutes = require('./routes/category');
+// const productRoutes = require('./routes/product');
+// const braintreeRoutes = require('./routes/braintree');
+// const orderRoutes = require('./routes/order');
 const postsRouter = require("./routes/posts");
 
 require('dotenv').config();
